d764ece7fc26976ee496b47626f63698af6b5a3b
Bavo De Bondt <Bavodebo@student.arteveldehs.be>
2025-03-04 13:20:05 +0100

    update

---
+++ server/routes.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response, Router } from "express";
+
+const router: Router = express.Router();
+
+// Homepagina
+router.get("/", (req: Request, res: Response): void => {
+  res.render("index", { title: "Quiz" });
+});
+
+//redirect get request op /quiz naar /
+router.get("/quiz", (req: Request, res: Response): void => {
+    res.redirect("/");
+  });
+
+// Quiz verwerken
+router.post("/quiz", (req: Request, res: Response): void => {
+  const correctAntwoord: string = "Parijs";
+  const userAntwoord: string = req.body.answer?.trim() || "";
+  const isCorrect: boolean = userAntwoord.toLowerCase() === correctAntwoord.toLowerCase();
+  const message: string = isCorrect ? "Correct! 🎉" : "Fout! 😢 Probeer opnieuw.";
+
+  res.render("result", { title: "Quiz resultaat", boodschap: message });
+});
+
+export default router;
